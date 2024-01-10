911f5a24f308e8a26c90c766cee57fef0dd7b5be
Sebastian <xenose@live.com>
2024-01-10 19:40:41 +0900

    removing more temporary files

--- old/tmp/assembly/source_models_models.o.asm
+++
@@ -1,6 +0,0 @@
-	.text
-	.intel_syntax noprefix
-	.file	"models.c"
-	.ident	"clang version 16.0.6"
-	.section	".note.GNU-stack","",@progbits
-	.addrsig
--- old/tmp/assembly/source_viper.o.asm
+++
@@ -1,6 +0,0 @@
-	.text
-	.intel_syntax noprefix
-	.file	"viper.c"
-	.ident	"clang version 16.0.6"
-	.section	".note.GNU-stack","",@progbits
-	.addrsig
