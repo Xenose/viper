aa5b3641f81ae1095e52ea80995f262173ed7f12
Sebastian <xenose@live.com>
2024-02-10 18:24:45 +0900

    fixed shader

--- viper/shaders/basic_vertex.glsl
+++ viper/shaders/basic_vertex.glsl
@@ -4,6 +4,6 @@ layout (location = 0) in vec3 aPos;
 out vec4 vertexColor;
 
 void main() {
-   gl_position = vec4(aPos, 1.0);
+   gl_Position = vec4(aPos, 1.0);
    vertexColor = vec4(0.5, 0.5, 0.0, 1.0);
 }
