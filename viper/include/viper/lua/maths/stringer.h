0a80a23e420183921e56bb413d30e01ad9b336e0
Sebastian <xenose@live.com>
2024-02-21 22:03:48 +0900

    Added more Lua utests

--- viper/include/viper/lua/maths/stringer.h
+++ viper/include/viper/lua/maths/stringer.h
@@ -6,15 +6,21 @@
 #include<lualib.h>
 
 /**
- * Wrapper function for the ViperUtoa functions so it can
+ * Wrapper function for the ViperUtoa function so it can
  * be called from Lua.
  */
 extern int ViperLuaUtoa(lua_State* lua);
 
 /**
- * Wrapper function for the ViperItoa functions so it can
+ * Wrapper function for the ViperItoa function so it can
  * be called from Lua.
  */
 extern int ViperLuaItoa(lua_State* lua);
+
+/**
+ * wrapper function for the ViperAtou function so it can
+ * be called from Lua.
+ */
+extern int ViperLuaAtou(lua_State* lua);
 
 #endif /* __header_viper_core_lua_maths_stringer__ */
