6b479b9610e980e1516c8290176df83f960588aa
xenose <xenose@live.com>
2024-02-22 18:26:38 +0900

    removed unneeded files

---
+++ scripts/check_deeps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+if command cloc > /dev/null 2&>1;  then
+	echo -e "[ \033[32mFOUND\033[0m ] cloc"
+fi
+
+#if command jq > /dev/null 2&>1;  then
+#	echo -e "[ \033[32mFOUND\033[0m ] jq"
+#fi
