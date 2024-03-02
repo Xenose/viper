6ce3cfedd0794d2874bb31d9dc157231c3f6c7d3
Sebastian <xenose@live.com>
2024-03-02 23:12:15 +0900

    added header with limits.h

---
+++ viper/include/viper/core/platform/limits.h
@@ -0,0 +1,10 @@
+#ifndef __header_viper_core_platform_limits__
+#define __header_viper_core_platform_limits__
+
+#ifndef _WIN32
+   #include<linux/limits.h>
+#else
+   #define PATH_MAX 32767
+#endif
+
+#endif /* __header_viper_core_platform_limits__ */
