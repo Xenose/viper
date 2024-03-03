8b91d25172d8eaeb831a9c0c1736f52491dda8c0
xenose <xenose@SAURON>
2024-03-03 23:28:51 +0900

    more windows code...

--- viper/source/core/file/dirent.c
+++ viper/source/core/file/dirent.c
@@ -1,9 +1,28 @@
+#include<viper/core/memory/allocator.h>
 #include<viper/core/file/dirent.h>
+#include<viper/core/debug/logger.h>
+
+#ifdef _WIN32
+    #include<Windows.h>
+#endif /* _WIN32 */
 
 #ifdef _WIN32
 
    DIR* opendir(cc* path) {
-      return NULL;
+       char buffer[MAX_PATH] = { 0 };
+       DIR* out = ViperCalloc(1, sizeof(DIR));
+
+       if (NULL == out) {
+           ViperLogError("Failed to allocate memory");
+           goto ERROR_EXIT;
+       }
+
+       strncpy(buffer, path, MAX_PATH);
+       strncat(buffer, "\\*", MAX_PATH);
+
+       return out;
+ERROR_EXIT:
+       return NULL;
    }
 
    struct dirent* readdir(DIR* dir) {
