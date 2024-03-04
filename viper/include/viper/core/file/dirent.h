0b2223d9075384158d12d9c84e4219c37d99b93a
xenose <xenose@SAURON>
2024-03-04 21:21:56 +0900

    adding more dirent.h functions to windows.

--- viper/include/viper/core/file/dirent.h
+++ viper/include/viper/core/file/dirent.h
@@ -7,22 +7,28 @@
 #ifndef _WIN32
    #include<dirent.h>
 #else
+	#include<Windows.h>
+
 	enum {
 		DT_UNKNOWN,
 		DT_REG,
 		DT_DIR,
 	};
 
-   typedef struct {
-   } DIR;
-
    struct dirent {
 	   ino_t d_ino;
 	   off_t d_off;
 	   unsigned short d_reclen;
 	   unsigned char d_type;
-	   char* d_name[NAME_MAX + 1];
+	   char d_name[NAME_MAX + 1];
    };
+
+   typedef struct {
+	   struct dirent ent;
+	   uint64_t count;
+	   WIN32_FIND_DATA findFileData;
+	   HANDLE handleFind;
+   } DIR;
 
    extern DIR* opendir(cc* path);
    extern struct dirent* readdir(DIR* dir);
--- viper/source/core/file/dirent.c
+++ viper/source/core/file/dirent.c
@@ -3,11 +3,10 @@
 #include<viper/core/debug/logger.h>
 
 #ifdef _WIN32
-    #include<Windows.h>
-#endif /* _WIN32 */
 
-#ifdef _WIN32
-
+    /**
+    * A windows implementation of opendir :: see [ man dirent.h ]
+    */
    DIR* opendir(cc* path) {
        char buffer[MAX_PATH] = { 0 };
        DIR* out = ViperCalloc(1, sizeof(DIR));
@@ -20,16 +19,45 @@
        strncpy(buffer, path, MAX_PATH);
        strncat(buffer, "\\*", MAX_PATH);
 
+       out->handleFind = FindFirstFile_T(buffer, &out->findFileData);
+
+       if (INVALID_HANDLE_VALUE == out->handleFind) {
+           goto ERROR_EXIT_FREE;
+       }
+
+       out->count = 0;
        return out;
+ERROR_EXIT_FREE:
+       free(out);
 ERROR_EXIT:
        return NULL;
    }
 
    struct dirent* readdir(DIR* dir) {
-      return NULL;
+       if (0 != dir->count++) {
+           int64_t re = FindNextFile(dir->handleFind, &dir->findFileData);
+
+           if (0 == re) {
+           }
+       }
+
+       dir->ent.d_ino = 0;
+
+       if (FILE_ATTRIBUTE_DIRECTORY & dir->findFileData.dwFileAttributes) {
+           dir->ent.d_type = DT_DIR;
+       } else if (FILE_ATTRIBUTE_NORMAL & dir->findFileData.dwFileAttributes) {
+           dir->ent.d_type = DT_REG;
+       } else {
+           dir->ent.d_type = DT_UNKNOWN;
+       }
+       
+       strncpy(dir->ent.d_name, dir->findFileData.cFileName, MAX_PATH);
+       return &dir->ent;
    }
    
    int closedir(DIR* dir) {
-      return 0;
+       FindClose(dir->handleFind);
+       free(dir);
+       return 0;
    }
 #endif /* _WIN32 */
